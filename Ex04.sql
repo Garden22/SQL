eab8e5200b7dfdbdaae97a81ff1a14f35f74944d
Garden22 <dalkomjjang@gmail.com>
2022-05-17 14:44:56 +0900

    220517

--- Ex04.sql
+++ Ex04.sql
@@ -33,7 +33,7 @@ WHERE salary < (SELECT AVG(salary)
 SELECT employee_id
        ,first_name
        ,salary
-FROM EMPLOYEES
+FROM employees
 WHERE salary IN (12008, 8300);
 
 SELECT employee_id
@@ -44,4 +44,73 @@ WHERE salary IN (SELECT salary
                  FROM employees
                  WHERE department_id = 110);
 -- IN 을 사용하면 해당하는 데이터를 모두 찾음
+
+
+-- 각 부서별로 최고급여를 받는 사원을 출력하세요
+SELECT department_id 부서명
+       ,employee_id 사번
+       ,first_name 이름
+       ,salary 최대급여
+FROM employees 
+WHERE (department_id, salary) IN (SELECT department_id
+                                         ,MAX(salary)
+                                  FROM employees
+                                  GROUP BY department_id)
+ORDER BY department_id ASC;
+
+
+-- 다중행 SUBQUERY ""ANY""
+-- OR 처럼 사용
+-- 부서번호가 110인 직원의 급여보다 큰 모든 직원의
+-- 사번, 이름, 급여를 출력하세요
+SELECT employee_id 사번
+       ,first_name 이름
+       ,salary 급여
+FROM employees
+WHERE salary > ANY (SELECT salary
+                    FROM employees
+                    WHERE department_id = 110);
+
+-- ""ALL""
+-- 이번에는 부서번호가 100인 모든 직원의 급여보다 큰 모든 직원의 급여 출력
+SELECT first_name 이름
+       ,salary 급여
+FROM employees
+WHERE salary > ALL (SELECT salary
+                    FROM employees
+                    WHERE department_id = 110);
+
+-------------------------------------------
+-- 각 부서별로 최고급여를 받는 사원을 출력하세요
+SELECT department_id 부서명
+       ,salary 급여
+FROM employees
+WHERE (department_id, salary) IN (SELECT department_id
+                                         ,MAX(salary)
+                                  FROM employees
+                                  GROUP BY department_id);
+
+
+SELECT e.department_id 부서아이디
+       ,d.department_name 부서명
+       ,e.first_name || ' ' || e.last_name 이름
+       ,e.salary 급여
+FROM employees e, departments d
+WHERE (e.department_id, e.salary) IN (SELECT department_id, MAX(salary)
+                                  FROM employees 
+                                  GROUP BY department_id)
+AND e.department_id = d.department_id
+ORDER BY e.department_id;
+
+
+-- 서브쿼리를 테이블로 사용할 수도 있음
+SELECT e.department_id 부서아이디
+       ,e.employee_id 사번
+       ,e.first_name 이름
+       ,e.salary 급여
+FROM employees e, (SELECT department_id, MAX(salary) salary
+                   FROM employees 
+                   GROUP BY department_id) s
+WHERE e.department_id = s.department_id
+AND e.salary = s.salary;
 
